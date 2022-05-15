cb00c0ebf5d1e3777627f7fdeeaa05db9b4ab7d5
George Beshay <90455303+GeorgeBeshay@users.noreply.github.com>
2022-05-15 20:34:39 +0200

    Add files via upload

--- Labs/Lab_7/DriverCode.java
+++ Labs/Lab_7/DriverCode.java
@@ -7,9 +7,11 @@ public class DriverCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		File myFile = new File("E:\\College\\CSED Level 1 2021 - 2022\\Second Term\\Data Structures 1\\Java\\src\\Labs\\Lab_7\\TestCaseB.txt");
+		
 		// Note: if the .txt file is in the same directory you are working in, no need to mention the exact file path.
 		MazeSolver myMazeSolver = new MazeSolver();
 		myMazeSolver.solveBFS(myFile);
+		myMazeSolver.solveDFS(myFile);
 		
 	}
 
--- Labs/Lab_7/MazeSolver.java
+++ Labs/Lab_7/MazeSolver.java
@@ -4,14 +4,133 @@ import java.awt.Point;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class MazeSolver{
+public class MazeSolver implements IMazeSolver{
 	
-//	@Override
+	@Override
 	public int[][] solveDFS(java.io.File maze){
+		
+		Scanner sc = null;
+		String tempS;
+		char[][] array_2d = null;
+		int n = 0, m = 0;
+		try {
+			sc = new Scanner(maze);
+			if(sc.hasNextLine()) {
+				tempS = sc.nextLine();
+				n = Integer.parseInt(tempS.charAt(0)+"");
+				m = Integer.parseInt(tempS.charAt(2)+"");
+				array_2d = new char[n][m];
+			}
+			while(sc.hasNext() && n > 0 && m > 0) {
+				for(int i = 0 ; i < n ; i++) {
+					tempS = sc.nextLine();
+					for(int j = 0 ; j < m ; j++) {
+						array_2d[i][j] = tempS.charAt(j);
+					}
+				}
+			}
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+		int[][] result=new int[50][2];
+		MyStack s= new MyStack(null,0);
+		int counter=0;
+		int i,j;
+		cell t =new cell();
+		
+		boolean[][] visited=new boolean[n][m];
+		
+		for(int k=0;k<n;k++) {
+			for(int l=0;l<m;l++) {
+				visited[k][l]=false;
+			}
+		}
+		
+		for(int k=0;k<n;k++) {
+			for(int l=0;l<m;l++) {
+				if(array_2d[k][l]=='S') {
+					s.push(k,l,'S',null);
+					visited[k][l]=true;
+					
+					while(!s.isEmpty()) {
+						t=s.pop();
+						visited[t.getI()][t.getJ()]=true;
+						
+						if(t.getA()=='.' || t.getA()=='S') {
+							result[counter][0]=t.getI();
+							result[counter][1]=t.getJ();
+							counter++;
+						}
+						
+						if(t.getA()=='E') {
+							result[counter][0]=t.getI();
+							result[counter][1]=t.getJ();
+							counter++;
+							
+							int[][] result_final=new int[counter][2];
+							for(int q=0;q<counter;q++) {result_final[q][0]=result[q][0];result_final[q][1]=result[q][1];}
+							
+							
+							System.out.print("DFS: ");
+							for(int h=0;h<result_final.length-1;h++) {
+								System.out.print("{" + result_final[h][0] + ", " + result_final[h][1] + "}, ");
+							}
+							System.out.print("{" + result_final[result_final.length-1][0] + ", " + result_final[result_final.length-1][1] + "}");
+							
+							return result_final;
+						}
+						
+						if(t.getA()=='.' || t.getA()=='S') {
+							i=t.getI();
+							j=t.getJ();
+							boolean child_found=false;
+							
+							if(j+1<m) {
+								if(visited[i][j+1]==false){
+									if(array_2d[i][j+1]=='.' || array_2d[i][j+1]=='E') {s.push(i, j+1, array_2d[i][j+1],t); child_found=true;}}
+								}
+								
+							if(i-1>=0) {
+								if(visited[i-1][j]==false){ 
+									if(array_2d[i-1][j]=='.' || array_2d[i-1][j]=='E') {s.push(i-1, j, array_2d[i-1][j],t); child_found=true;}}
+							}
+							if(j-1>=0) {
+								if(visited[i][j-1]==false) {
+									if(array_2d[i][j-1]=='.'|| array_2d[i][j-1]=='E') {s.push(i,j-1,array_2d[i][j-1],t); child_found=true;}}
+							}
+							if(i+1<n) {
+								if(visited[i+1][j]==false) {
+									if(array_2d[i+1][j]=='.'|| array_2d[i+1][j]=='E') {s.push(i+1, j, array_2d[i+1][j],t); child_found=true;}}
+							}
+							if(child_found==false) {
+								cell temp = s.peek().getParent();
+								int i_temp= temp.getI();
+								int j_temp= temp.getJ();
+								for(int d=0;d<counter;d++) {
+									if(result[d][0]==i_temp && result[d][1]==j_temp) {counter=d+1;}
+								}
+							}
+						}
+						
+						
+					}
+				}
+			}
+		}
+		
 		return null;
+		}
+		catch(Exception e) {
+			System.out.print("Error");
+			return null;
+
+		}
 	}
 	
-//	@Override
+	
+	@Override
 	public int[][] solveBFS(java.io.File maze){
 		
 		Scanner sc = null;
@@ -37,10 +156,6 @@ public class MazeSolver{
 		} catch(FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-		
 		
 		LLQueue myQueue = new LLQueue();
 		Stack myStack = new Stack();
@@ -87,7 +202,7 @@ public class MazeSolver{
 			System.out.print("{" +tempNewPRO.x + ", " + tempNewPRO.y +  "}, ");
 		}
 		tempNewPRO = myStack.pop();
-		System.out.print("{" +tempNewPRO.x + ", " + tempNewPRO.y +  "}");
+		System.out.print("{" +tempNewPRO.x + ", " + tempNewPRO.y +  "}\n");
 		ArrayOfPoints[ArrayOfPointsIndex] = tempNewPRO;
 		return convertFromArrOfPointsToArrOfIntegers(ArrayOfPoints);
 	}
